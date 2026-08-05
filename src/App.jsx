c5acfc04daf6798bc3a6be6b2c616cc3ca43a7a3
mikerobinson0991292 <mikerobinson0991292@gmail.com>
2026-08-06 02:17:35 +0300

    edit file

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/home/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
-import NewUser from "./pages/newUser/newUser";
+import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
@@ -13,51 +13,30 @@ import Login from "./pages/login/Login";
 import { useSelector } from "react-redux";
 
 function App() {
-    const admin = useSelector((state) => state.user.currentUser?.isAdmin);
+  const admin = useSelector((state) => state.user.currentUser?.isAdmin);
 
-    return (
-        <BrowserRouter>
-            {admin && <Topbar />}
+  return (
+    <BrowserRouter>
+      {admin && <Topbar />}
 
-            <div className="container">
-                {admin && <Sidebar />}
-                <Routes>
-                    <Route
-                        path="/"
-                        element={admin ? <Home /> : <Login />}
-                    />
-                    {admin && (
-                        <>
-                            <Route
-                                path="/users"
-                                element={<UserList />}
-                            />
-                            <Route
-                                path="/user/:userId"
-                                element={<User />}
-                            />
-                            <Route
-                                path="/newUser"
-                                element={<NewUser />}
-                            />
-                            <Route
-                                path="/products"
-                                element={<ProductList />}
-                            />
-                            <Route
-                                path="/product/:productId"
-                                element={<Product />}
-                            />
-                            <Route
-                                path="/newProduct"
-                                element={<NewProduct />}
-                            />
-                        </>
-                    )}
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
+      <div className="container">
+        {admin && <Sidebar />}
+        <Routes>
+          <Route path="/" element={admin ? <Home /> : <Login />} />
+          {admin && (
+            <>
+              <Route path="/users" element={<UserList />} />
+              <Route path="/user/:userId" element={<User />} />
+              <Route path="/newUser" element={<NewUser />} />
+              <Route path="/products" element={<ProductList />} />
+              <Route path="/product/:productId" element={<Product />} />
+              <Route path="/newProduct" element={<NewProduct />} />
+            </>
+          )}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
