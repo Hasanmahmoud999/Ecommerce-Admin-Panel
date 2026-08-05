f16db9384701806cbeca7621cd19129f46e3f0af
mikerobinson0991292 <mikerobinson0991292@gmail.com>
2026-08-06 02:21:58 +0300

    edit file

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL;
 //  const TOKEN =  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser?.accessToken;
 // const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser ? JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken : "";
 const getToken = () => {
